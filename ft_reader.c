c42d2fa6e2a80f3562557ea1885a0a1c3ee85872
tmanet <tmanet@student.42.fr>
2016-03-29 12:36:42 +0200

    ajout des struct

--- ft_reader.c
+++ ft_reader.c
@@ -1,36 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lem.h                                              :+:      :+:    :+:   */
+/*   ft_reader.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/03/25 15:14:36 by tmanet            #+#    #+#             */
-/*   Updated: 2016/03/29 12:08:53 by tmanet           ###   ########.fr       */
+/*   Created: 2016/03/29 12:08:19 by tmanet            #+#    #+#             */
+/*   Updated: 2016/03/29 12:10:03 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LEM_H
-# define LEM_H
-# include "libft/libft.h"
+#include "lem.h"
 
-typedef struct s_room		t_room;
-typedef struct s_anthill	t_anthill;
-
-struct						s_anthill
+void	ft_reader(t_anthill *ah, int argc, char **argv)
 {
-	int			ants;
 
 
-};
-
-struct						s_room
-{
-	char		*name;
-	int			begin;
-	int			end;
 
 
-};
-
-#endif
+}
