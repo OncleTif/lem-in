eb494858afafb1e0e0d8f16df29108bdcb02e26b
tmanet <tmanet@student.42.fr>
2016-03-30 16:20:49 +0200

    ajout de ft_com

--- ft_line_reader.c
+++ ft_line_reader.c
@@ -1,21 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_com.c                                           :+:      :+:    :+:   */
+/*   ft_line_reader.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/03/30 16:12:15 by tmanet            #+#    #+#             */
-/*   Updated: 2016/03/30 16:20:03 by tmanet           ###   ########.fr       */
+/*   Created: 2016/03/30 15:47:05 by tmanet            #+#    #+#             */
+/*   Updated: 2016/03/30 16:20:16 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem.h"
 
-void	ft_com(t_anthill *ah, char *str)
+int	ft_line_reader(t_anthill *ah)
 {
-	if (ft_strequ(str, "##start"))
-		ah->next_start = 1;
-	else if (ft_strequ(str, "##end"))
-		ah->next_end = 1;
+char	*str;
+
+if ((get_next_line(0, &str)) < 1)
+	return (0);
+if (str[0] == '#')
+	ft_com(ah, str);
+return (1);
 }
