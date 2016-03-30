f5af06245e6b8b277dd82d415659c85190b0678c
tmanet <tmanet@student.42.fr>
2016-03-30 18:35:57 +0200

    ajout de ants number

--- ft_line_reader.c
+++ ft_line_reader.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/30 15:47:05 by tmanet            #+#    #+#             */
-/*   Updated: 2016/03/30 17:59:26 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/03/30 18:35:10 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,17 @@ int	ft_line_reader(t_anthill *ah)
 		return (0);
 	if (str[0] == '#')
 		ft_com(ah, str);
-	else
+	else if (ah->not_first)
 	{
 		if ((end = ft_strchr(str, ' ')))
 			ret = ft_add_room(ah, ft_strsub(str, 0, end - str));
 		else
 			ret = ft_add_link(ah, str);
+	}
+	else
+	{
+		ah->ants = ft_atoi_strict(str);
+		ah->not_first = 1;
 	}
 	ft_strdel(&str);
 	return (ret);
